1e7880052b1b44664a2ad27bee8044590162beaa
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-01-17 15:00:09 -0800

    Created student and advisor classes

---
+++ NestedObjects/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+	/// <summary>
+	/// Represents individual college student
+	/// </summary>
+	internal class Student
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public DateOnly DateOfBirth { get; set; }
+		public string SchoolEmail { get; set; }
+		public string Phone { get; set; }
+		public Advisor Advisor { get; set; }
+	}
+}
